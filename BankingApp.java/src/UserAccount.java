f9d74570e06cb8e8214bc167954d37e2304bc7b3
AnathiNkohla <168439956+AnathiNkohla@users.noreply.github.com>
2024-10-31 13:48:30 +0200

    Add files via upload
    Java Application

---
+++ BankingApp.java/src/UserAccount.java
@@ -0,0 +1,27 @@
+
+public class UserAccount {
+    final int accountNumber;
+    final  String accountHolder;
+    private double balance;
+
+    public UserAccount(int accountNumber, String accountHolder, double balance){
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+    public void deposit(double amount){
+        balance += amount;
+        System.out.println("Deposit successful");
+    }
+    public void withdraw(double amount){
+        if (amount <= balance) {
+            balance -= amount;
+            System.out.println("withdrawal successful");
+        }else{
+            System.out.println("Insufficient funds");
+        }
+    }
+    public double getBalance(){
+        return balance;
+    }
+}
